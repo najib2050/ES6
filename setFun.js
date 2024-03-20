16cbce43732aa0af85c95f5f377861b754d33a8d
najib2050 <abdiwelinoor2@gmail.com>
2024-03-21 00:13:34 +0300

    ES6 set constructor

---
+++ setFun.js
@@ -0,0 +1,40 @@
+/*
+set -> is a collection of new values
+      each value can olny occur once
+      a set can hold any value of any data type
+
+new set() is a constructor;
+
+
+ */
+//How to create a set
+//1: passing an array to new set()
+let constacts=new Set(["najiib","alii","abdii","2767643"])
+// console.log(constacts);
+//2: create a new set and use add() to add new values
+let fruits=new Set()
+fruits.add("banana");
+fruits.add("mango")
+// console.log(fruits);
+//3:create a new set and use add() to add new variables;
+const room=new Set() // create set
+let a="a"; //creating varaibels
+let b="b";
+let c= "c";
+
+room.add(a); //adding variables to set 
+room.add(b)
+room.add(c)
+// console.log(room);
+
+
+
+
+
+function order(name,multiplication){
+      console.log(name,multiplication(4,6));
+
+}
+order("ali noor",function(a,b){
+      return a*b
+})
